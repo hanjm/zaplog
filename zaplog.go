4ec7b334a26bfe968ab5f12dde7bcbb6c7c6764c
hanjm <hanjinming@outlook.com>
2017-05-22 22:58:58 +0800

    set milliSecondsDurationEncoder to default DurationEncoder

--- zaplog.go
+++ zaplog.go
@@ -21,8 +21,8 @@ func timeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
 	enc.AppendString(t.Format("2006-01-02 15:04:05"))
 }
 
-// millionSecondsDurationEncoder serializes a time.Duration to a floating-point number of micro seconds elapsed.
-func millionSecondsDurationEncoder(d time.Duration, enc zapcore.PrimitiveArrayEncoder) {
+// milliSecondsDurationEncoder serializes a time.Duration to a floating-point number of micro seconds elapsed.
+func milliSecondsDurationEncoder(d time.Duration, enc zapcore.PrimitiveArrayEncoder) {
 	enc.AppendFloat64(float64(d) / float64(time.Millisecond))
 }
 
@@ -34,7 +34,7 @@ func newLoggerConfig(debugLevel bool, te zapcore.TimeEncoder, de zapcore.Duratio
 		loggerConfig.EncoderConfig.EncodeTime = te
 	}
 	if de == nil {
-		loggerConfig.EncoderConfig.EncodeDuration = millionSecondsDurationEncoder
+		loggerConfig.EncoderConfig.EncodeDuration = milliSecondsDurationEncoder
 	} else {
 		loggerConfig.EncoderConfig.EncodeDuration = de
 	}
